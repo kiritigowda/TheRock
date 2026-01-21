bca64a391ba3cb786bbf5357448286f19fabb955
kiritigowda <kiritigowda@gmail.com>
2026-01-21 15:22:28 -0800

    Fix - Test scripts

--- build_tools/github_actions/test_executable_scripts/test_rocjpeg.py
+++ build_tools/github_actions/test_executable_scripts/test_rocjpeg.py
@@ -15,7 +15,8 @@ if THEROCK_BIN_DIR_STR is None:
     sys.exit(1)
 THEROCK_BIN_DIR = Path(THEROCK_BIN_DIR_STR)
 SCRIPT_DIR = Path(__file__).resolve().parent
-THEROCK_DIR = SCRIPT_DIR.parent.parent
+THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
+THEROCK_TEST_DIR = Path(THEROCK_DIR) / "build"
 
 ROCJPEG_TEST_PATH = str(Path(THEROCK_BIN_DIR).parent / "share" / "rocjpeg" / "test")
 if not os.path.isdir(ROCJPEG_TEST_PATH):
@@ -36,20 +37,20 @@ def setup_env(env):
         if "LD_LIBRARY_PATH" in env:
             env["LD_LIBRARY_PATH"] = f"{HIP_LIB_PATH}:{env['LD_LIBRARY_PATH']}"
         else:
-            env["LD_LIBRARY_PATH"] = HIP_LIB_PATH
+            env["LD_LIBRARY_PATH"] = str(HIP_LIB_PATH)
     else:
         logging.info(f"++ rocjpeg tests only supported on Linux")
         exit()
 
 def execute_tests(env):
-    ROCJPEG_TEST_DIR = Path(THEROCK_DIR) / "rocjpeg-test"
+    ROCJPEG_TEST_DIR = Path(THEROCK_TEST_DIR) / "rocjpeg-test"
     cmd = [
         "mkdir",
         "-p",
         "rocjpeg-test",
     ]
-    logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
-    subprocess.run(cmd, cwd=THEROCK_DIR, check=True, env=env)
+    logging.info(f"++ Exec [{THEROCK_TEST_DIR}]$ {shlex.join(cmd)}")
+    subprocess.run(cmd, cwd=THEROCK_TEST_DIR, check=True, env=env)
 
     cmd = [
         "cmake",
@@ -57,14 +58,13 @@ def execute_tests(env):
     ]
     logging.info(f"++ Exec [{ROCJPEG_TEST_DIR}]$ {shlex.join(cmd)}")
     subprocess.run(cmd, cwd=ROCJPEG_TEST_DIR, check=True, env=env)
-    
+
     cmd = [
         "ctest",
         "--output-on-failure",
     ]
     logging.info(f"++ Exec [{ROCJPEG_TEST_DIR}]$ {shlex.join(cmd)}")
     subprocess.run(cmd, cwd=ROCJPEG_TEST_DIR, check=True, env=env)
-
 
 if __name__ == "__main__":
     setup_env(env)
