kiritigowda/TheRock

commit 5f60db78b53ca25f8bd6299d31f29a8b13791ed9

Add tests for rocprofiler-compute (#2300)
## Motivation

<!-- Explain the purpose of this PR and the goals it aims to achieve.
-->
Add test coverage for `rocprofiler-compute` component

## Technical Details

<!-- Explain the changes along with any relevant GitHub links. -->
- Updated `CMakeLists.txt` for `rocprofiler-compute` to build and
install tests
- Updated `artifact-rocprofiler-sdk.toml` to include necessary test
requirements for rocprofiler-compute under the test artifact for sdk
- Updated `fetch_test_configurations.py` to include
`rocprofiler-compute`, added `requirements-files` parameter to pass
along to `test_component.yml` to install pip reqs (compute requires
additional pip requirements to run)
- Updated `test_component.yml` to run a setup step before the test step
that will run `install_requirements.py` to install additional
`requirements.txt` files as needed for tests
- Updated `cmake/therock_amdgpu_targets.cmake` to exclude
rocprofiler-compute for unsupported devices
- Add new file`test_rocprofiler_compute.py` to run ctest on compute
tests

## Test Plan

<!-- Explain any relevant testing done to verify this PR. -->
- Ensure tests pass locally when running in the no_rocm Docker image and
using the generated artifacts from this branch
- Ensure tests pass in CI pipeline

## JIRA ID

ROCM-324

## Test Result

<!-- Briefly summarize test outcomes. -->
- Tests pass locally and in the pipeline
- Smoke tests take around 10 minutes to complete. I tried adding running
them in parallel but that led to some flakiness on the profiling tests.
  - Full test suite run take 30-40 minutes
- The `test_profile_pc_sampling` test has been excluded from running due
to its constant failing right now due to a rocr issue (see Jira
mentioned in comment in `test_rocprofiler_compute.py`), this can be
added back once it is confirmed to be fixed

## Submission Checklist

- [x] Look over the contributing guidelines at

cmake/therock_amdgpu_targets.cmake
@@ -48,6 +48,7 @@ therock_add_amdgpu_target(gfx906 "Radeon VII / MI50 CDNA" FAMILY dgpu-all gfx906
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     composable_kernel # https://github.com/ROCm/TheRock/issues/1245
     rocWMMA # https://github.com/ROCm/TheRock/issues/1944
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 
 # gfx908 (separate family - different instruction support from gfx906/gfx90a)
@@ -75,6 +76,7 @@ therock_add_amdgpu_target(gfx1010 "AMD RX 5700" FAMILY dgpu-all gfx101X-all gfx1
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     composable_kernel # https://github.com/ROCm/TheRock/issues/1245
     rocWMMA # https://github.com/ROCm/TheRock/issues/1944
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 therock_add_amdgpu_target(gfx1011 "AMD Radeon Pro V520" FAMILY dgpu-all gfx101X-all gfx101X-dgpu
   EXCLUDE_TARGET_PROJECTS
@@ -82,13 +84,16 @@ therock_add_amdgpu_target(gfx1011 "AMD Radeon Pro V520" FAMILY dgpu-all gfx101X-
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     composable_kernel # https://github.com/ROCm/TheRock/issues/1245
     rocWMMA # https://github.com/ROCm/TheRock/issues/1944
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
+
 therock_add_amdgpu_target(gfx1012 "AMD RX 5500" FAMILY dgpu-all gfx101X-all gfx101X-dgpu
   EXCLUDE_TARGET_PROJECTS
     hipBLASLt # https://github.com/ROCm/TheRock/issues/1062
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     composable_kernel # https://github.com/ROCm/TheRock/issues/1245
     rocWMMA # https://github.com/ROCm/TheRock/issues/1944
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 
 # gfx103X family
@@ -97,6 +102,7 @@ therock_add_amdgpu_target(gfx1030 "AMD RX 6800 / XT" FAMILY dgpu-all gfx103X-all
     hipBLASLt # https://github.com/ROCm/TheRock/issues/1062
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     rocWMMA # https://github.com/ROCm/TheRock/issues/1944
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 therock_add_amdgpu_target(gfx1031 "AMD RX 6700 / XT" FAMILY dgpu-all gfx103X-all gfx103X-dgpu
   EXCLUDE_TARGET_PROJECTS
@@ -109,6 +115,7 @@ therock_add_amdgpu_target(gfx1032 "AMD RX 6600" FAMILY dgpu-all gfx103X-all gfx1
     hipBLASLt # https://github.com/ROCm/TheRock/issues/1062
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     rocWMMA # https://github.com/ROCm/TheRock/issues/1944
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 therock_add_amdgpu_target(gfx1033 "AMD Van Gogh iGPU" FAMILY igpu-all gfx103X-all gfx103X-igpu
   EXCLUDE_TARGET_PROJECTS
@@ -128,31 +135,37 @@ therock_add_amdgpu_target(gfx1035 "AMD Radeon 680M Laptop iGPU" FAMILY igpu-all 
     hipBLASLt # https://github.com/ROCm/TheRock/issues/1062
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     rocWMMA # https://github.com/ROCm/TheRock/issues/1944
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 therock_add_amdgpu_target(gfx1036 "AMD Raphael iGPU" FAMILY igpu-all gfx103X-all gfx103X-igpu
   EXCLUDE_TARGET_PROJECTS
     hipBLASLt # https://github.com/ROCm/TheRock/issues/1062
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     rocWMMA # https://github.com/ROCm/TheRock/issues/1944
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 
 # gfx110X family
 therock_add_amdgpu_target(gfx1100 "AMD RX 7900 XTX" FAMILY dgpu-all gfx110X-all gfx110X-dgpu
   EXCLUDE_TARGET_PROJECTS
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 therock_add_amdgpu_target(gfx1101 "AMD RX 7800 XT" FAMILY dgpu-all gfx110X-all gfx110X-dgpu
   EXCLUDE_TARGET_PROJECTS
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 therock_add_amdgpu_target(gfx1102 "AMD RX 7700S/Framework Laptop 16" FAMILY dgpu-all gfx110X-all gfx110X-dgpu
   EXCLUDE_TARGET_PROJECTS
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 therock_add_amdgpu_target(gfx1103 "AMD Radeon 780M Laptop iGPU" FAMILY igpu-all gfx110X-all gfx110X-igpu
   EXCLUDE_TARGET_PROJECTS
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     rccl  # https://github.com/ROCm/TheRock/issues/150
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 
 # gfx115X family
@@ -160,31 +173,37 @@ therock_add_amdgpu_target(gfx1150 "AMD Strix Point iGPU" FAMILY igpu-all gfx115X
   EXCLUDE_TARGET_PROJECTS
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     rccl  # https://github.com/ROCm/TheRock/issues/150
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 therock_add_amdgpu_target(gfx1151 "AMD Strix Halo iGPU" FAMILY igpu-all gfx115X-all gfx115X-igpu
   EXCLUDE_TARGET_PROJECTS
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     rccl  # https://github.com/ROCm/TheRock/issues/150
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 therock_add_amdgpu_target(gfx1152 "AMD Krackan 1 iGPU" FAMILY igpu-all gfx115X-all gfx115X-igpu
   EXCLUDE_TARGET_PROJECTS
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     rccl  # https://github.com/ROCm/TheRock/issues/150
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 therock_add_amdgpu_target(gfx1153 "AMD Radeon 820M iGPU" FAMILY igpu-all gfx115X-all gfx115X-igpu
   EXCLUDE_TARGET_PROJECTS
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     rccl  # https://github.com/ROCm/TheRock/issues/150
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 
 # gfx120X family
 therock_add_amdgpu_target(gfx1200 "AMD RX 9060 / XT" FAMILY dgpu-all gfx120X-all
   EXCLUDE_TARGET_PROJECTS
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 therock_add_amdgpu_target(gfx1201 "AMD RX 9070 / XT" FAMILY dgpu-all gfx120X-all
   EXCLUDE_TARGET_PROJECTS
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 
 # Optional extension targets (used for out of tree target development).

.github/workflows/test_component.yml
@@ -128,6 +128,12 @@ jobs:
         run: |
           python ./build_tools/print_driver_gpu_info.py
 
+      - name: Setup Additional Requirements
+        if: ${{ fromJSON(inputs.component).additional_requirements_files != '' }}
+        run: |
+          python ./build_tools/install_additional_requirements.py \
+            --requirements-files=${{ join(fromJSON(inputs.component).additional_requirements_files, ',') }}
+
       - name: Test
         timeout-minutes: ${{ fromJSON(inputs.component).timeout_minutes }}
         env:

build_tools/github_actions/fetch_test_configurations.py
@@ -384,6 +384,19 @@ test_matrix = {
             "windows": 2,
         },
     },
+    # rocprofiler-compute tests
+    "rocprofiler-compute": {
+        "job_name": "rocprofiler_compute",
+        "fetch_artifact_args": "--rocprofiler-compute --rocprofiler-sdk --tests",
+        "timeout_minutes": 60,
+        "additional_requirements_files": [
+            "libexec/rocprofiler-compute/requirements.txt",
+            "libexec/rocprofiler-compute/requirements-test.txt",
+        ],
+        "test_script": f"python {_get_script_path('test_rocprofiler_compute.py')} -v",
+        "platform": ["linux"],
+        "total_shards": 2,
+    },
     # libhipcxx hipcc tests
     "libhipcxx_hipcc": {
         "job_name": "libhipcxx_hipcc",

build_tools/github_actions/test_executable_scripts/test_rocprofiler_compute.py
@@ -1,0 +1,89 @@
+import logging
+import os
+import shlex
+import subprocess
+from pathlib import Path
+
+# Resolve paths
+THEROCK_BIN_DIR = os.getenv("THEROCK_BIN_DIR")
+OUTPUT_ARTIFACTS_DIR = os.getenv("OUTPUT_ARTIFACTS_DIR")
+SCRIPT_DIR = Path(__file__).resolve().parent
+THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
+
+THEROCK_BIN_PATH = Path(THEROCK_BIN_DIR).resolve()
+THEROCK_PATH = THEROCK_BIN_PATH.parent
+THEROCK_LIB_PATH = str(THEROCK_PATH / "lib")
+ROCPROFILER_COMPUTE_DIRECTORY = THEROCK_PATH / "libexec" / "rocprofiler-compute"
+
+# Set up ROCM_PATH
+environ_vars = os.environ.copy()
+environ_vars["ROCM_PATH"] = str(THEROCK_PATH)
+
+# Set up PATH
+old_path = os.getenv("PATH", "")
+rocm_bin = str(THEROCK_BIN_PATH)
+if old_path:
+    environ_vars["PATH"] = f"{rocm_bin}:{old_path}"
+else:
+    environ_vars["PATH"] = rocm_bin
+
+# Set up LD_LIBRARY_PATH
+old_ld_lib_path = os.getenv("LD_LIBRARY_PATH", "")
+sysdeps_path = f"{THEROCK_LIB_PATH}/rocm_sysdeps/lib"
+if old_ld_lib_path:
+    environ_vars["LD_LIBRARY_PATH"] = (
+        f"{THEROCK_LIB_PATH}:{sysdeps_path}:{old_ld_lib_path}"
+    )
+else:
+    environ_vars["LD_LIBRARY_PATH"] = f"{THEROCK_LIB_PATH}:{sysdeps_path}"
+
+# Set up excluded tests (include Jiras)
+# AIPROFSDK-36: rocr issue causing test to fail
+EXCLUDED_TESTS = [
+    "test_profile_pc_sampling",
+]
+
+# Sharding
+shard_index = int(os.getenv("SHARD_INDEX", "1")) - 1
+total_shards = int(os.getenv("TOTAL_SHARDS", "1"))
+
+# Smoke Tests Setup
+SMOKE_TESTS = [
+    "test_autogen_config",
+    "test_utils",
+    "test_num_xcds_cli_output",
+    "test_num_xcds_spec_class",
+    "test_L1_cache_counters",
+    "test_analyze_workloads",
+    "test_analyze_commands",
+    "test_metric_validation",
+    "test_profile_iteration_multiplexing_1",
+]
+
+# Run tests
+cmd = [
+    "ctest",
+    "--test-dir",
+    f"{ROCPROFILER_COMPUTE_DIRECTORY}",
+    "--output-on-failure",
+    "--verbose",
+    "--exclude-regex",
+    f"{"|".join(EXCLUDED_TESTS)}",
+    "--tests-information",
+    f"{shard_index},,{total_shards}",
+]
+
+# If smoke tests are enabled, we run smoke tests only.
+# Otherwise, we run the normal test suite
+test_type = os.getenv("TEST_TYPE", "full")
+if test_type == "smoke":
+    cmd.append("--tests-regex")
+    cmd.append("|".join(SMOKE_TESTS))
+
+logging.info(f"++ Exec [{THEROCK_PATH}]$ {shlex.join(cmd)}")
+subprocess.run(
+    cmd,
+    cwd=THEROCK_PATH,
+    check=True,
+    env=environ_vars,
+)

build_tools/install_additional_requirements.py
@@ -1,0 +1,52 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+import os
+import shlex
+import subprocess
+from pathlib import Path
+import sys
+
+SCRIPT_DIR = Path(__file__).resolve().parent
+THEROCK_DIR = SCRIPT_DIR.parent
+THEROCK_OUTPUT_DIR = str(THEROCK_DIR / "build")
+
+
+def install_requirements(input: str):
+    environ_vars = os.environ.copy()
+
+    requirements_files = input.split(",")
+
+    for file in requirements_files:
+        cmd = [
+            "uv",
+            "pip",
+            "install",
+            "-r",
+            f"{THEROCK_OUTPUT_DIR}/{file}",
+        ]
+        logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
+        subprocess.run(cmd, cwd=THEROCK_DIR, check=True, env=environ_vars)
+
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--requirements-files",
+        type=str,
+        default="",
+        help="A comma separated list of requirements.txt files to install",
+    )
+    args = parser.parse_args(argv)
+    if not args.requirements_files:
+        logging.info(
+            "No requirements file(s) provided. Exiting install_additional_requirements.py..."
+        )
+        sys.exit(0)
+
+    install_requirements(str(args.requirements_files))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])

build_tools/install_rocm_from_artifacts.py
@@ -32,6 +32,7 @@ python build_tools/install_rocm_from_artifacts.py
     [--rocdecode | --no-rocdecode]
     [--rocjpeg | --no-rocjpeg]
     [--rocprofiler-compute | --no-rocprofiler-compute]
+    [--rocprofiler-sdk | --no-rocprofiler-sdk ]
     [--rocprofiler-systems | --no-rocprofiler-systems]
     [--rocrtst | --no-rocrtst]
     [--rocwmma | --no-rocwmma]
@@ -352,6 +353,7 @@ def retrieve_artifacts_by_run_id(args):
             args.rocdecode,
             args.rocjpeg,
             args.rocprofiler_compute,
+            args.rocprofiler_sdk,
             args.rocprofiler_systems,
             args.rocrtst,
             args.rocwmma,
@@ -752,16 +754,16 @@ def main(argv):
     )
 
     artifacts_group.add_argument(
-        "--rocprofiler-systems",
+        "--rocprofiler-sdk",
         default=False,
-        help="Include 'rocprofiler-systems' artifacts",
+        help="Include 'rocprofiler-sdk' artifacts",
         action=argparse.BooleanOptionalAction,
     )
 
     artifacts_group.add_argument(
-        "--rocprofiler-sdk",
+        "--rocprofiler-systems",
         default=False,
-        help="Include 'rocprofiler-sdk' artifacts",
+        help="Include 'rocprofiler-systems' artifacts",
         action=argparse.BooleanOptionalAction,
     )
 

profiler/CMakeLists.txt
@@ -122,8 +122,13 @@ endif(THEROCK_BUILD_TESTING)
     EXTERNAL_SOURCE_DIR "${THEROCK_ROCM_SYSTEMS_SOURCE_DIR}/projects/rocprofiler-compute"
     BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/rocprofiler-compute"
     BACKGROUND_BUILD
+    DEFAULT_GPU_TARGETS
+      gfx942
     CMAKE_ARGS
       -DHIP_PLATFORM=amd
+      -DENABLE_TESTS=${THEROCK_BUILD_TESTING}
+      -DINSTALL_TESTS=${THEROCK_BUILD_TESTING}
+      -DTEST_FROM_INSTALL=${THEROCK_BUILD_TESTING}
       -DCHECK_PYTHON_DEPS=OFF
     CMAKE_INCLUDES
       therock_explicit_finders.cmake
@@ -193,6 +198,7 @@ endif(THEROCK_BUILD_TESTING)
       doc
       lib
       run
+      test
     SUBPROJECT_DEPS
       aqlprofile
       rocprofiler-sdk
@@ -214,8 +220,6 @@ endif(THEROCK_BUILD_TESTING)
       aqlprofile
       rocprofiler-sdk
       rocprofiler-compute
-      roctracer
-      ${_rocprofiler_sdk_optional_deps}
   )
 
   ##############################################################################

commit 874ddcb532b45d77d634e6f4719d589a8bc2034f

Disable rocprofiler-compute in all gfx103X targets (#3676)
## Motivation

<!-- Explain the purpose of this PR and the goals it aims to achieve.
-->
gfx103X builds appear to be failing since #2300 got merged. This should
address these build errors and unblock gfx103X releases.

## Technical Details

<!-- Explain the changes along with any relevant GitHub links. -->
- rocprofiler-compute does not support gfx103X architectures, so we
added the project to the `EXCLUDE_TARGET_PROJECTS` list for the gfx103X
family in `therock_amdgpu_targets.cmake`
- It appears that `EXCLUDE_TARGET_PROJECTS` entries for
rocprofiler-compute gfx1031, gfx1033, and gfx1034 were missing
- This was due to #1629 being merged two weeks ago, and the changes
related to `therock_amdgpu_targets.cmake` in the #2300 PR happening
before this new inclusion
- Failures were not found before merging due to PR not running gfx103X
builds

## Test Plan

<!-- Explain any relevant testing done to verify this PR. -->
Ensure that gfx103X builds are able to pass without any errors from
rocprofiler-compute

## Test Result

<!-- Briefly summarize test outcomes. -->
- Ran a manual gfx103X workflow with these changes:
https://github.com/ROCm/TheRock/actions/runs/22497338111/job/65175257319

## Submission Checklist

- [X] Look over the contributing guidelines at
https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

cmake/therock_amdgpu_targets.cmake
@@ -112,6 +112,7 @@ therock_add_amdgpu_target(gfx1031 "AMD RX 6700 / XT" FAMILY dgpu-all gfx103X-all
   hipBLASLt # https://github.com/ROCm/TheRock/issues/1062
   hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
   rocWMMA # https://github.com/ROCm/TheRock/issues/1944
+  rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 therock_add_amdgpu_target(gfx1032 "AMD RX 6600" FAMILY dgpu-all gfx103X-all gfx103X-dgpu
   EXCLUDE_TARGET_PROJECTS
@@ -125,6 +126,7 @@ therock_add_amdgpu_target(gfx1033 "AMD Van Gogh iGPU" FAMILY igpu-all gfx103X-al
     hipBLASLt # https://github.com/ROCm/TheRock/issues/1062
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     rocWMMA # https://github.com/ROCm/TheRock/issues/1944
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
     composable_kernel
 )
 therock_add_amdgpu_target(gfx1034 "AMD RX 6500 XT" FAMILY dgpu-all gfx103X-all gfx103X-dgpu
@@ -132,6 +134,7 @@ therock_add_amdgpu_target(gfx1034 "AMD RX 6500 XT" FAMILY dgpu-all gfx103X-all g
     hipBLASLt # https://github.com/ROCm/TheRock/issues/1062
     hipSPARSELt # https://github.com/ROCm/TheRock/issues/2042
     rocWMMA # https://github.com/ROCm/TheRock/issues/1944
+    rocprofiler-compute # https://github.com/ROCm/TheRock/issues/2892
 )
 therock_add_amdgpu_target(gfx1035 "AMD Radeon 680M Laptop iGPU" FAMILY igpu-all gfx103X-all gfx103X-igpu
   EXCLUDE_TARGET_PROJECTS