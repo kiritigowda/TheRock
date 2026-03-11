1053a8bbe5a3f2fc41658841e544027c3e54faa6
dileepr1 <163147013+dileepr1@users.noreply.github.com>
2026-03-11 09:24:26 -0400

    Changes to integrate Test Filter Standardization for miopen in TheRock (#3595)
    ## Motivation
    
    This PR is to integrate the Test Filter Standardization POC done in
    MIOpen to TheRock
    Once active, the MIOpen CI tests will be triggered through this.
    Other components will follow.
    
    ## Technical Details
    
    The POC in MIOpen is done here:
    https://github.com/ROCm/rocm-libraries/pull/3513.
    This PR adds test_runner.py as a generic runner to call the required
    test categories from MIOpen, and can be expanded to other components.
    
    ## Test Plan
    
    Make sure that MIOpen tests are getting triggered and run fine through TheRock CI.

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -285,7 +285,7 @@ test_matrix = {
         "job_name": "miopen",
         "fetch_artifact_args": "--blas --miopen --rand --tests",
         "timeout_minutes": 60,
-        "test_script": f"python {_get_script_path('test_miopen.py')}",
+        "test_script": f"python {_get_script_path('test_runner.py')}",
         "platform": ["linux", "windows"],
         "total_shards_dict": {
             "linux": 4,
